0cb9e6f1cae598f9d1714093c99d8b059367fb0e
flpdsc <92yjkim@gmail.com>
2021-07-26 19:53:33 +0900

    0726

---
+++ c/19.c
@@ -0,0 +1,25 @@
+//분노 유발자
+#include <stdio.h>
+int main(void)
+{
+    int n, i, cnt=0, h[101], max;
+
+    scanf("%d", &n);
+
+    for(i=1; i<=n; ++i){
+        scanf("%d", &h[i]);
+    }
+
+    max = h[n];
+
+    for(i=n-1; i>=1; --i){
+        if(h[i] > max){
+            max = h[i];
+            cnt++;
+        }
+    }
+
+    printf("%d\n", cnt);
+
+    return 0;
+}
