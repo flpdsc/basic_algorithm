40645996951fe923f2fe0b29df04585a6a2499f9
flpdsc <92yjkim@gmail.com>
2021-09-04 22:51:55 +0900

    0904 부분집합(DFS)

---
+++ cpp/59.cpp
@@ -0,0 +1,26 @@
+//부분집합(DFS)
+#include <iostream>
+using namespace std;
+int n, ch[11];
+void DFS(int L)
+{
+    if(L==n+1){
+        for(int i=1; i<=n; ++i){
+            if(ch[i]==1) cout << i << " ";
+        }
+        puts("");
+    }
+    else{
+        ch[L]=1;
+        DFS(L+1);
+        ch[L]=0;
+        DFS(L+1);
+    }
+}
+
+int main()
+{
+    cin >> n;
+    DFS(1);
+    return 0;
+}
