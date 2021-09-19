f9c46b5497eb29e1baccb791c48f335b5bbc5f56
flpdsc <92yjkim@gmail.com>
2021-09-19 22:32:50 +0900

    0919 최소힙(priority_queue : 우선순위 큐)

---
+++ cpp/74.cpp
@@ -0,0 +1,23 @@
+//최소힙(priority_queue : 우선순위 큐)
+#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+    freopen("../data/73_input.txt", "rt", stdin);
+    int n;
+    priority_queue<int> pQ;
+    while(1){
+        cin >> n;
+        if(n==-1) break;
+        else if(n==0){
+            if(pQ.empty()) cout << -1 << endl;
+            else{
+                cout << -pQ.top() << endl;
+                pQ.pop();
+            }
+        }
+        else pQ.push(-n);
+    }
+    return 0;
+}
