5b0f11e534c9c6184e9682ff1117e39c8544a103
flpdsc <92yjkim@gmail.com>
2021-10-15 15:58:15 +0900

    1015 최대점수 구하기(냅색 알고리즘)

---
+++ c/101.c
@@ -0,0 +1,19 @@
+//최대점수 구하기(냅색 알고리즘)
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    freopen("../data/101_input.txt", "rt", stdin);
+    int n, m, s, t, *dy;
+    scanf("%d %d", &n, &m);
+    dy = (int*)calloc(m+1, sizeof(int));
+    for(int i=0; i<n; ++i){
+        scanf("%d %d", &s, &t);
+        for(int j=t; j<=m; ++j){
+            if(dy[j]<dy[j-t]+s) dy[j] = dy[j-t]+s;
+        }
+    }
+    printf("%d\n", dy[m]);
+    free(dy);
+    return 0;
+}
---
+++ cpp/101.cpp
@@ -0,0 +1,20 @@
+//최대점수 구하기(냅색 알고리즘)
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    freopen("../data/101_input.txt", "rt", stdin);
+    int n, m, s, t;
+    cin >> n >> m;
+    vector<int> dy(m+1);
+    for (int i=0; i<n; ++i){
+        cin >> s >> t;
+        for(int j=t; j<=m; ++j){
+            dy[j] = max(dy[j], dy[j-t]+s);
+        }
+    }
+    cout << dy[m] << endl;
+    return 0;
+}
